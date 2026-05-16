5f891e98ffb30c0786c44a94639d79d3eb01e3ef
Sayed Galib Hossen Rizvi <166820079+galibrezvi99@users.noreply.github.com>
2026-05-16 02:35:35 +0200

    Add files via upload

---
+++ Lab01_VHDL_Intro/half_adder.vhd
@@ -0,0 +1,19 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity half_adder is
+    Port (
+        A     : in STD_LOGIC;
+        B     : in STD_LOGIC;
+        SUM   : out STD_LOGIC;
+        CARRY : out STD_LOGIC
+    );
+end half_adder;
+
+architecture Behavioral of half_adder is
+begin
+
+    SUM   <= A XOR B;
+    CARRY <= A AND B;
+
+end Behavioral;
