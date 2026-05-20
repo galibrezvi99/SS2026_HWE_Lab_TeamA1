589f07de41fac9cdc850ad33532745dbfdfc4eb9
islamriajul2599 <islamriajul2599@gmail.com>
2026-05-20 12:31:31 +0200

    Refactor full subtractor VHDL code structure

--- Labs/Lab1/Task5/full_subtractor_bhv.vhd
+++ Labs/Lab1/Task5/full_subtractor_bhv.vhd
@@ -21,8 +21,8 @@
 --   BOUT = (not A and B) or (BIN and not (A xor B))
 --
 -- Note:
---   File name contains bhv because lab asks for this name.
---   Code is structural because that is the requested method.
+--  
+--   Code is structural.
 -- ============================================================
 
 
